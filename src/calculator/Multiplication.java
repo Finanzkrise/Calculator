e25384fbe02e5e3c96dd54daf782039d9b2072cd
Finanzkrise <benjamin.2327@hotmail.com>
2021-07-21 04:19:40 +0200

    started implementing multiplication 0,x * 0,x  - OK x,0 * 0,x  - OK x,0 * x,0  - OK 0,x * x,0  - NOK

--- src/calculator/Multiplication.java
+++ src/calculator/Multiplication.java
@@ -26,7 +26,7 @@ public class Multiplication extends Operation implements ListLocation {
     }
 
 
-    public int getDigitRightOfComma(int index, Decimal number, int location) {
+    public int getDigit(int index, Decimal number, int location) {
         int digit = 0;
         if (isIndexInBounds(index, number, location)) {
             digit = number.getNumberList().get(location).get(index);
@@ -34,10 +34,6 @@ public class Multiplication extends Operation implements ListLocation {
         return digit;
     }
 
-    @Override
-    public int getDigitLeftOfComma(int index, Decimal number, int location) {
-        return super.getDigitLeftOfComma(index, number, location);
-    }
 
     private Decimal multiplyNumbers(Decimal number1, Decimal number2) {
 
@@ -48,107 +44,138 @@ public class Multiplication extends Operation implements ListLocation {
             // multiply each digit of number1RightOfComma with number2RightOfComma[numberTwoIndex]
             // 0,x * 0,x
             resultRightOfComma = new ArrayList<>();
-            int numberOneIndex = 0;
-            for (numberOneIndex = 0; number1.getNumberList().get(RIGHT_OF_COMMA).size() > numberOneIndex; numberOneIndex++) {
+
+            for (int numberOneIndex = 0; number1.getNumberList().get(RIGHT_OF_COMMA).size() > numberOneIndex; numberOneIndex++) {
                 int tempResult = multiplyTwoDigits(
-                        getDigitRightOfComma(number1.getNumberList().get(RIGHT_OF_COMMA).size() - 1 - numberOneIndex, number1, RIGHT_OF_COMMA),
-                        getDigitRightOfComma(numberTwoIndex, number2, RIGHT_OF_COMMA)
+                        getDigit(number1.getNumberList().get(RIGHT_OF_COMMA).size() - 1 - numberOneIndex, number1, RIGHT_OF_COMMA),
+                        getDigit(numberTwoIndex, number2, RIGHT_OF_COMMA)
                 );
-                System.out.println("number 1 "+getDigitRightOfComma(number1.getNumberList().get(RIGHT_OF_COMMA).size() - 1 - numberOneIndex, number1, RIGHT_OF_COMMA));
-                System.out.println("number 2 " + getDigitRightOfComma(numberTwoIndex, number2, RIGHT_OF_COMMA));
+                System.out.println("number 1 "+ getDigit(number1.getNumberList().get(RIGHT_OF_COMMA).size() - 1 - numberOneIndex, number1, RIGHT_OF_COMMA));
+                System.out.println("number 2 " + getDigit(numberTwoIndex, number2, RIGHT_OF_COMMA));
                 System.out.println("result " + tempResult);
                 getResultRightOfComma().add(0, tempResult);
-
-                System.out.println("result " + tempResult);
                 System.out.println(numberOneIndex +1 + ". Durchlauf - klein");
             }
-                for (int i = 0; numberTwoIndex >= i; i++ ){
-                    getResultRightOfComma().add(0, multiplyTwoDigits(0, 0));
-                }
+            for (int i = 0; numberTwoIndex >= i; i++ ){
+                getResultRightOfComma().add(0, multiplyTwoDigits(0, 0));
+            }
 
             System.out.println("right of comma * right of comma " + new Decimal(true, getResultRightOfComma()));
             decimalList.add(new Decimal(true, getResultRightOfComma()));
+
+
+
+
+
+
 
 
             // multiply each digit of number1LeftOfComma with number2RightOfComma[numberTwoIndex]
             //x,0 * 0,x
             resultRightOfComma = new ArrayList<>();
             resultLeftOfComma = new ArrayList<>();
-            for (numberOneIndex = number1.getNumberList().get(LEFT_OF_COMMA).size(); 0 <= numberOneIndex; numberOneIndex--) {
-                int i;
-                int tempResult = multiplyTwoDigits(getDigitLeftOfComma(numberOneIndex, number1, LEFT_OF_COMMA), getDigitRightOfComma(numberTwoIndex, number2, RIGHT_OF_COMMA));
+            for (int numberOneIndex = 0; number1.getNumberList().get(LEFT_OF_COMMA).size() > numberOneIndex; numberOneIndex++) {
 
-                //System.out.println("digit to be multiplied " + getDigitLeftOfComma(numberOneIndex, number1, LEFT_OF_COMMA));
-                // System.out.println("temp result: " + tempResult);
-//_____________
-                i = numberTwoIndex;
-                if (numberTwoIndex >= i) {
+                int tempResult = multiplyTwoDigits(
+                        getDigit(number1.getNumberList().get(LEFT_OF_COMMA).size() - 1 - numberOneIndex, number1, LEFT_OF_COMMA),
+                        getDigit(numberTwoIndex, number2, RIGHT_OF_COMMA)
+                );
+                System.out.println(getDigit(number1.getNumberList().get(LEFT_OF_COMMA).size() - 1 - numberOneIndex, number1, LEFT_OF_COMMA) + " * " + getDigit(numberTwoIndex, number2, RIGHT_OF_COMMA));
+
+                if (numberOneIndex <= numberTwoIndex) {
                     getResultRightOfComma().add(0, tempResult);
-                    i++;
+
                 } else {
                     getResultLeftOfComma().add(0, tempResult);
                 }
-                resultLeftOfComma.add(0, multiplyTwoDigits(0, 0));
+                System.out.println(numberOneIndex +1 + ". Durchlauf - klein: " + getResultLeftOfComma() + ", " + getResultRightOfComma());
 
             }
-            //System.out.println("left of comma * right of comma " + new Decimal(resultLeftOfComma, resultRightOfComma));
+            resultLeftOfComma.add(0, multiplyTwoDigits(0, 0));
+            System.out.println("left of comma * right of comma " + new Decimal(resultLeftOfComma, resultRightOfComma));
             decimalList.add(new Decimal(resultLeftOfComma, resultRightOfComma));
             System.out.println(numberTwoIndex +1 + ". Durchlauf - groß");
         }
 
 
+
+
+
+        // for length of number2 Left of Comma
         for (int numberTwoIndex = 0; number2.getNumberList().get(LEFT_OF_COMMA).size() > numberTwoIndex; numberTwoIndex++) {
+
+
+
+
             // multiply each digit of number1RightOfComma with number2LeftOfComma[numberTwoIndex]
+            //0,x * x,0
             resultRightOfComma = new ArrayList<>();
             resultLeftOfComma = new ArrayList<>();
-            for (int numberOneIndex = number2.getNumberList().get(LEFT_OF_COMMA).size(); 0 <= numberOneIndex; numberOneIndex--) {
-                int i = 0;
-                int tempResult = multiplyTwoDigits(getDigitRightOfComma(numberOneIndex, number1, RIGHT_OF_COMMA), getDigitRightOfComma(numberTwoIndex, number2, LEFT_OF_COMMA));
-                if (i < number2.getNumberList().get(LEFT_OF_COMMA).size()) {
+            for (int numberOneIndex = 0; number1.getNumberList().get(RIGHT_OF_COMMA).size() > numberOneIndex; numberOneIndex++) {
+
+                int tempResult = multiplyTwoDigits(
+                        getDigit(number1.getNumberList().get(RIGHT_OF_COMMA).size() - 1 - numberOneIndex, number1, RIGHT_OF_COMMA),
+                        getDigit(numberTwoIndex, number2, LEFT_OF_COMMA)
+                );
+                System.out.println(getDigit(number1.getNumberList().get(RIGHT_OF_COMMA).size() - 1 - numberOneIndex, number1, RIGHT_OF_COMMA) + " * " + getDigit(numberTwoIndex, number2, LEFT_OF_COMMA));
+
+                if (numberOneIndex <= numberTwoIndex) {
                     getResultRightOfComma().add(0, tempResult);
+
                 } else {
-                    getResultLeftOfComma().add(tempResult);
+                    getResultLeftOfComma().add(0, tempResult);
                 }
+                System.out.println(numberOneIndex +1 + ". Durchlauf - klein: " + getResultLeftOfComma() + ", " + getResultRightOfComma());
+
             }
-            System.out.println("right of comma * left of comma " + new Decimal(resultLeftOfComma, resultRightOfComma));
+            resultLeftOfComma.add(0, multiplyTwoDigits(0, 0));
+            System.out.println("left of comma * right of comma " + new Decimal(resultLeftOfComma, resultRightOfComma));
             decimalList.add(new Decimal(resultLeftOfComma, resultRightOfComma));
+            System.out.println(numberTwoIndex +1 + ". Durchlauf - groß");
+
+
+
+
+
+
+
+
+
+
 
 
             // multiply each digit of number1LeftOfComma with number2LeftOfComma[numberTwoIndex]
+            // x,0 * x,0
             resultLeftOfComma = new ArrayList<>();
-            // multiply each digit of number1LeftOfComma with number2LeftOfComma[numberTwoIndex]
-            for (int numberOneIndex = number2.getNumberList().get(LEFT_OF_COMMA).size(); 0 <= numberOneIndex; numberOneIndex--) {
-                int tempResult = multiplyTwoDigits(getDigitRightOfComma(numberOneIndex, number1, LEFT_OF_COMMA), getDigitRightOfComma(numberTwoIndex, number2, LEFT_OF_COMMA));
+            // verbessern
+            for (int numberOneIndex = 0; number1.getNumberList().get(LEFT_OF_COMMA).size() > numberOneIndex; numberOneIndex++) {
+                int tempResult = multiplyTwoDigits(
+                    getDigit(number1.getNumberList().get(LEFT_OF_COMMA).size() -1-numberOneIndex, number1, LEFT_OF_COMMA),
+                    getDigit(numberTwoIndex, number2, LEFT_OF_COMMA)
+                );
+                System.out.println(getDigit(number1.getNumberList().get(LEFT_OF_COMMA).size() - 1 - numberOneIndex, number1, LEFT_OF_COMMA) + " * " + getDigit(numberTwoIndex, number2, LEFT_OF_COMMA));
+                System.out.println("result " + tempResult);
                 getResultLeftOfComma().add(0, tempResult);
+                System.out.println(numberOneIndex +1 + ". Durchlauf - klein: " + getResultLeftOfComma());
             }
-            for (int k = number2.getNumberList().get(LEFT_OF_COMMA).size() - numberTwoIndex - 1; k > 0; k--) {
+
+            getResultLeftOfComma().add(0, multiplyTwoDigits(0,0));
+            for (int i = number2.getNumberList().get(LEFT_OF_COMMA).size() -1 - numberTwoIndex; 0 < i; i--){
                 getResultLeftOfComma().add(0);
             }
+
+            System.out.println("Durchlauf - klein: " + getResultLeftOfComma());
             System.out.println("left of comma * left of comma " + new Decimal(false, getResultLeftOfComma()));
             decimalList.add(new Decimal(false, getResultLeftOfComma()));
         }
-
 
         for (Decimal tempResult : decimalList) {
             System.out.println("addition");
             tempResultForAddition = new Addition().operate(tempResultForAddition, tempResult);
         }
-
         return tempResultForAddition;
-
     }
 
-    /*
-        private void multiplyNumbersOfNumberOne(Decimal number1, Decimal number2) {
-            int lengthOfLongerNumber = getLengthOfLongerNumberSection(number1, number2, LEFT_OF_COMMA);
-            for (int i = 0; lengthOfLongerNumber > i; i++) {
-                int tempResult = multiplyTwoDigits(getDigitAfterCommaAt(i, number1, LEFT_OF_COMMA), getDigitAfterCommaAt(i, number2, LEFT_OF_COMMA));
-                getResultLeftOfComma().add(0, tempResult);
-            }
-        }
-
-
-     */
     private int multiplyTwoDigits(int number1, int number2) {
         int product = number1 * number2 + overflow;
         int result = product % 10;
