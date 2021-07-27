cdb842b2f49ff1e628e79e2ceba38c1f7d2f94d9
Finanzkrise <benjamin.2327@hotmail.com>
2021-07-28 01:45:05 +0200

    adjusting for nachkommastellen waiting for RootExtraction

--- src/calculator/operations/Exponentiation.java
+++ src/calculator/operations/Exponentiation.java
@@ -22,12 +22,32 @@ public class Exponentiation extends  Operation implements ListLocation {
     }
 
     private Decimal exponantiate(Decimal number1, Decimal number2) {
-        //Decimal zero = new Decimal("0,0");
-        while (!isDecimalHigherThanDecimal(new Decimal("2"), number2)) {
-            number2 = new Subtraction().operate(number2, new Decimal("1"));
-            number1 = new Multiplication().operate(number1, number1);
-            System.out.println("tempresult: " + number1);
+        Decimal result;
+        Decimal root = new Decimal("1");
+
+        // adjust for right of comma - * 10^i
+        for (int i = 0; number2.getNumberList().get(RIGHT_OF_COMMA).size() > i; i++) {
+            number2 = new Multiplication(number2, new Decimal("10")).getResult();
+            root = new Multiplication(root, new Decimal("10")).getResult();
         }
-        return number1;
+
+        if (number2.toString().equals("0,")) {
+            result = new Division(number1, number1).getResult();
+        }
+        else if (number2.toString().equals("1,")) {
+            result = number1;
+        }
+        else{
+            while (!isDecimalHigherThanDecimal(new Decimal("2"), number2)) {
+                number2 = new Subtraction().operate(number2, new Decimal("1"));
+                number1 = new Multiplication().operate(number1, number1);
+                System.out.println("tempresult: " + number1);
+            }
+            result = new RootExtraction(number1, root).getResult();
+        }
+        if (!number1.isNumberPositive()) {
+            result.setIsPositive(false);
+        }
+        return result;
     }
 }
