1d77a0e54427f2dc8675de1e09cb50feb05e32eb
Finanzkrise <benjamin.2327@hotmail.com>
2021-07-27 03:19:48 +0200

    implemented generatePrime

---
+++ src/calculator/operations/RootExtraction.java
@@ -0,0 +1,52 @@
+package calculator.operations;
+
+import calculator.Decimal;
+import calculator.ListLocation;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class RootExtraction extends Operation implements ListLocation {
+
+    Logger logger = LogManager.getLogger(RootExtraction.class);
+
+    @Override
+    Decimal operate(Decimal number1, Decimal number2) {
+
+        return null;
+    }
+
+    @Override
+    void executeOperation(Decimal number1, Decimal number2) {
+
+    }
+
+   public Decimal generatePrime(int count){
+        Decimal result = null;
+        int prime = 3;
+        int divisor = 3;
+        int primesFound = 1;
+        if (count == 1) {
+            return new Decimal("2");
+        }
+        while (count > primesFound) {
+            logger.info(primesFound + " of " + count + " Primes found");
+            while (prime % divisor != 0) {
+                divisor += 2;
+                logger.info(prime + " % " + divisor + " = " + prime % divisor);
+            }
+            if (prime == divisor) {
+                logger.info(prime + " % " + divisor + " = " + prime % divisor);
+                divisor = 3;
+                primesFound++;
+                result = new Decimal(String.valueOf(prime));
+                logger.info("Prime found: " + prime);
+                prime += 2;
+            }else{
+                prime += 2;
+                divisor = 3;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/calculator/test/RootExtractionTest.java
@@ -0,0 +1,55 @@
+package calculator.test;
+
+import calculator.Decimal;
+import calculator.operations.RootExtraction;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RootExtractionTest {
+
+    @Test
+    void generatePrimeFirstTest() {
+        Decimal prime = new RootExtraction().generatePrime(1);
+        assertEquals(2, prime.getNumberList().get(0).get(0));
+    }
+
+    @Test
+    void generatePrimeSecondTest() {
+        Decimal prime = new RootExtraction().generatePrime(2);
+        assertEquals(3, prime.getNumberList().get(0).get(0));
+    }
+
+    @Test
+    void generatePrimeThirdTest() {
+        Decimal prime = new RootExtraction().generatePrime(3);
+        assertEquals(5, prime.getNumberList().get(0).get(0));
+    }
+
+    @Test
+    void generatePrimeFourthTest() {
+        Decimal prime = new RootExtraction().generatePrime(4);
+        assertEquals(7, prime.getNumberList().get(0).get(0));
+    }
+
+    @Test
+    void generatePrimeTenTest() {
+        Decimal prime = new RootExtraction().generatePrime(10);
+        assertEquals(2, prime.getNumberList().get(0).get(0));
+        assertEquals(9, prime.getNumberList().get(0).get(1));
+    }
+
+    @Test
+    void generatePrime25Test() {
+        Decimal prime = new RootExtraction().generatePrime(25);
+        assertEquals(9, prime.getNumberList().get(0).get(0));
+        assertEquals(7, prime.getNumberList().get(0).get(1));
+    }
+
+    @Test
+    void generatePrimeHundredTest() {
+        Decimal prime = new RootExtraction().generatePrime(100);
+        assertEquals(5, prime.getNumberList().get(0).get(0));
+        assertEquals(4, prime.getNumberList().get(0).get(1));
+        assertEquals(1, prime.getNumberList().get(0).get(2));
+    }
+}
